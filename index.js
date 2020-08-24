8740b6a821db141edd08b4ffe2c32e45f186ef52
Thomas Kretschmann <t.kretschmann@t-online.de>
2020-08-25 01:58:42 +0200

    code alot shorter

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 //* STYLE
-const styleElem = document.head.appendChild(document.createElement("style"));
+const styleElem = document.head.appendChild(document.createElement("style"))
 styleElem.innerHTML = `
     .anchor {
-        position: absolute; 
-        // padding-right: 2px; 
+        position: absolute;
         transform: translateX(-100%) translateY(0.1em);
         display: none;
     }
@@ -12,7 +11,7 @@ styleElem.innerHTML = `
     h3:hover .anchor {
         display: inline;
     }
-`;
+`
 
 //* SCRIPT
 //! svg icon from: https://icons.getbootstrap.com/ thank you! 👌
@@ -23,14 +22,13 @@ const icon = `
         <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 0 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 0 0-4.243-4.243L6.586 4.672z"/>
         <path d="M10 9.5a2.99 2.99 0 0 0 .288-1.46l-.167.167a1.99 1.99 0 0 1-.896.518 1.99 1.99 0 0 1-.518.896l-.167.167A3.004 3.004 0 0 0 10 9.501z"/>
     </svg>
-`;
+`
 
-document.querySelectorAll("h2:not(#mw-toc-heading), h3").forEach((headline) => {
+document.querySelectorAll(".mw-headline").forEach((headline) => {
+    const link = document.createElement("a")
+    link.href = "#" + headline.getAttribute("id")
+    link.classList.add("anchor")
 
-    const link = document.createElement("a");
-    link.href = `${window.location.href.split("#")[0]}#${headline.firstChild.getAttribute("id")}`;
-    link.classList.add("anchor");
-
-    link.insertAdjacentHTML("afterbegin", icon);
-    headline.insertBefore(link, headline.firstChild);
-});
+    link.insertAdjacentHTML("afterbegin", icon)
+    headline.insertBefore(link, headline.firstChild)
+})
