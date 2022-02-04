e6b3e78c4561d4e73ac73ffe4f37c37c2349cfc8
Thomas Kretschmann <t.kretschmann@t-online.de>
2022-02-04 12:22:47 +0100

    added support fpr chrome based browsers

--- options/options.js
+++ options/options.js
@@ -1,26 +1,35 @@
-// restore settings
-document.addEventListener("DOMContentLoaded", () => browser.storage.local.get("links").then((result) => {
-    // success
-    if (result.links === "icons" || (result.links !== "icons" && result.links !== "underlined")) {
-        document.querySelector("#iconsRadioBtn").checked = true
+//* restore settings
+document.addEventListener("DOMContentLoaded", () =>
+  // suitable for Firefox and Chrome
+  chrome.storage.local.get(["links"], (result) => {
+    if (
+      result.links === "icons" ||
+      (result.links !== "icons" && result.links !== "underlined")
+    ) {
+      document.querySelector("#iconsRadioBtn").checked = true;
     } else if (result.links === "underlined") {
-        document.querySelector("#underlinedRadioBtn").checked = true
+      document.querySelector("#underlinedRadioBtn").checked = true;
     }
-}, (error) => {
-    // error
-    console.log(`Error: ${error}`)
-}))
+  })
+);
 
-// add listener
-document.querySelector("#iconsRadioBtn").addEventListener("change", saveOptions)
-document.querySelector("#underlinedRadioBtn").addEventListener("change", saveOptions)
+//* add listener
+document
+  .querySelector("#iconsRadioBtn")
+  .addEventListener("change", saveOptions);
+document
+  .querySelector("#underlinedRadioBtn")
+  .addEventListener("change", saveOptions);
 
-// save settings
+//* save settings
 function saveOptions(e) {
-    e.preventDefault()
-    document.querySelector("#info").style.display = "block"
-    const settingsObject = {
-        links: document.querySelector("#iconsRadioBtn").checked ? "icons" : "underlined",
-    }
-    browser.storage.local.set(settingsObject)
+  e.preventDefault();
+  document.querySelector("#info").style.display = "block";
+  const settingsObject = {
+    links: document.querySelector("#iconsRadioBtn").checked
+      ? "icons"
+      : "underlined",
+  };
+  // suitable for Firefox and Chrome
+  chrome.storage.local.set(settingsObject);
 }
